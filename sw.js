b77eb17c830407c7af6137bd7eb1e81099fa9c28
LordBoby-Crypto <zacmarlin4@gmail.com>
2026-07-15 12:00:34 -0500

    Cache SN-Scan v3 staged-old workflow

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'sn-scan-v3';
-const APP_FILES = ['./', './index.html', './styles.css', './upgrade-v2.js', './serial-parser.js', './ocr-multipass.js', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'sn-scan-v4';
+const APP_FILES = ['./', './index.html', './styles.css', './upgrade-v2.js', './upgrade-v3-addon.js', './serial-parser.js', './ocr-multipass.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_FILES)));
